71b15ed489e24ee9fe4fd3b2a884921d17ee2c52
Donnacha OSullivan <donnachaosullivan1@gmail.com>
2025-10-29 14:36:01 +0000

    Added generate_pr_description and print output to terminal

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 $env:Path += ";C:\Program Files\Git\cmd"
+
+# Useful git commands
+
+- git status
+- git add FILE1 FILE2
+- git commit -m "Description of my new code changes"
+
+# Other useful commands for the project
+- git diff > diff.txt
+
+# Task 1
+- There is a directory called `input` that contains a `diff.txt` file.
+- The goal is to feed this `diff.txt` file to the LLM and generate a PR description.
+- The PR description should be saved in a folder called `output` in a new file called `PR_DESCRIPTION.md`
--- src/main.py
+++ src/main.py
@@ -5,22 +5,35 @@ import os
 load_dotenv()
 
 
-def generate_unicorn_story() -> str:
-    api_key = os.getenv("KEY")
-    client = OpenAI(api_key=api_key)
+def generate_pr_description(client: OpenAI) -> str:
+    with open("input/diff.txt", "r") as f:
+        diff = f.read()
+
+    prompt = f"""
+        "You are a helpful assistant that generates PR descriptions."
+        "# comment to LLM
+        "# the text below is the diff of code changes. summarise what changed and why.
+        {diff}
+        "please write a clear and professional PR description in markdown format."""
     
     response = client.responses.create(
         model="gpt-5",
-        input="Write a one-sentence bedtime story about a unicorn."
+        input=prompt
     )
     return response.output_text
 
 
 def main():
-    print("Hello from kreoh-TY-project!")
-    story = generate_unicorn_story()
+    api_key = os.getenv("KEY")
+    client = OpenAI(api_key=api_key)
+    story = generate_pr_description(client)
     print(story)
 
 
+
 if __name__ == "__main__":
-    main()
+    main() 
+
+
+
+
